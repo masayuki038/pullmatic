88fec53b5c4f11cf44fad7bd2acde9a876ea869d
Masayuki Takahashi <masayuki038@gmail.com>
2015-09-20 11:47:08 +0000

    Add os_info, hostname, selinux to Server

--- lib/pullmatic/resource/server.rb
+++ lib/pullmatic/resource/server.rb
@@ -8,7 +8,10 @@ module Pullmatic
 
       def execute
         os_info = Specinfra.backend.os_info
-        {:os_info  => os_info}
+        se_cmd = Specinfra.backend.command.get(:check_selinux_has_mode, "enabled")
+        hostname = host_inventory['hostname']
+        selinux = (Specinfra.backend.run_command(se_cmd).exit_status == 0) ? "enabled" : "disabled"
+        {:server => {:os_info => os_info, :hostname => hostname, :selinux => selinux}}
       end
     end
   end
--- spec/lib/pullmatic/resource/server_spec.rb
+++ spec/lib/pullmatic/resource/server_spec.rb
@@ -5,9 +5,11 @@ module Pullmatic
     describe Server do
       describe ".execute" do
         it "should generate server" do
-          expect(described_class.execute).to eq({:os_info => {:family => "ubuntu",
-                                                              :release => "14.04",
-                                                              :arch => "x86_64"}})
+          expect(described_class.execute).to eq({:server => {:os_info => {:family => "ubuntu",
+                                                                         :release => "14.04",
+                                                                         :arch => "x86_64"},
+                                                    :hostname => "a3b6ee4e9425",
+                                                    :selinux => "disabled"}})
         end
       end
     end
